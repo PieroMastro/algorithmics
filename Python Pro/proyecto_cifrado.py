c495e0bf0312a68f5774c5740ebcfbb0ba43a216
PieroMastro <pier.andres@gmail.com>
2023-12-26 03:44:34 +0000

    Added cifrado Cesar como funcion

--- Python Pro/proyecto_cifrado.py
+++ Python Pro/proyecto_cifrado.py
@@ -1,3 +1,5 @@
+# CIFRADO CESAR:
+
 text = input('Ingrese una cadena de texto:')
 shift = 3
 
@@ -13,3 +15,24 @@ for char in text.lower():
         encrypted_text += alphabet[new_index]
 print('plain text:', text)
 print('encrypted text:', encrypted_text)
+
+# 1.1 DEFINIDO CON FUNCION:
+
+text = input('Ingrese un texto a encriptar: ')
+shift = input('Ingrese un numero (entre 1 y 26): ')
+
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
+
+caesar(text, shift)
