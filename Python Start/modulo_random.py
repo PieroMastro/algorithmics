4e1a416d329e270f56c8e5b2eb321d42a2f54412
PieroMastro <pier.andres@gmail.com>
2024-01-08 15:54:02 +0000

    Added random module to python start

---
+++ Python Start/modulo_random.py
@@ -0,0 +1,25 @@
+import random
+from random import randint
+from random import *
+
+# random_num = randint(1, 10)
+# print(random_num)
+# print(random())
+
+# PROGRAMA PARA CALCULAR SORTEO DE EQUIPOS ALEATORIAMENTE:
+# total = int(input('El numero de equipos: '))
+# participant_1 = randint(1, total)
+# participant_2 = randint(1, total)
+
+# print(f'Equipo {participant_1} vs Equipo {participant_2}')
+
+#PROGRAMA PARA CALCULAR DISTRIBUCION DE JUGADORES A LOS EQUIPOS DE MANERA ALEATORIA:
+# name = input('Nombre de la persona (off - finalizar): ')
+
+# while name != 'off':
+#     equipo = randint(1,2)
+#     print(name, ', equipo', equipo)
+#     name = input('Nombre de la persona (off - finalizar): ')
+# print('Equipos completados')
+
+
