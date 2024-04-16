277e9a617224201f9ddd0e7cc92fff60af9b10de
PieroMastro <pier.andres@gmail.com>
2024-04-16 20:56:53 +0000

    refactoring/fixes to catch_up.py

--- Python Start II/M5L1 Intro to Pygame/catch_up.py
+++ Python Start II/M5L1 Intro to Pygame/catch_up.py
@@ -2,20 +2,21 @@ from pygame import *
 
 init()
 
-#crear ventana de juego
+# definiendo constantes
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 blue_x, blue_y = 100, 400
 pink_x, pink_y = 600, 400
-speed = 10
+SPEED = 10
 
+# crear ventana de juego
 window = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 display.set_caption('Catch Up!')
 
-#establecer fondo de la escena
+# establecer fondo de la escena
 background = image.load('background.png')
 background_img = transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-#crear 2 objetos y colocarlos en la escena
+#c rear 2 objetos y colocarlos en la escena
 blue = transform.scale(image.load('sprite1.png'), (100, 100))
 pink = transform.scale(image.load('sprite2.png'), (100, 100))
 pink = transform.flip(pink, True, False)
@@ -23,38 +24,40 @@ pink = transform.flip(pink, True, False)
 running = True
 clock = time.Clock()
 
+# estableciendo el ciclo de juego
 while running:
-#manejo de evento “clic en “Cerrar ventana”
+# manejo de evento “clic en “Cerrar ventana”
     for e in event.get():
         if e.type == QUIT:
             running = False    
 
+    # "pintando objetos en pantalla"
     window.blit(background_img, (0, 0))
     window.blit(blue, (blue_x, blue_y))
     window.blit(pink, (pink_x, pink_y))
 
     keys = key.get_pressed()
-    #movimiento sprite "blue"
+    # movimiento sprite "blue"
     if keys[K_a] and blue_x > 5:
-        blue_x -= speed
+        blue_x -= SPEED
     if keys[K_d] and blue_x < SCREEN_WIDTH - 105:
-        blue_x += speed
+        blue_x += SPEED
     if keys[K_w] and blue_y > 5:
-        blue_y -= speed
+        blue_y -= SPEED
     if keys[K_s] and blue_y < SCREEN_HEIGHT - 105:
-        blue_y += speed
+        blue_y += SPEED
         
-    #movimiento sprite "pink"
+    # movimiento sprite "pink"
     if keys[K_LEFT] and pink_x > 5:
-        pink_x -= speed
+        pink_x -= SPEED
     if keys[K_RIGHT] and pink_x < SCREEN_WIDTH - 105:
-        pink_x += speed
+        pink_x += SPEED
     if keys[K_UP] and pink_y > 5:
-        pink_y -= speed
+        pink_y -= SPEED
     if keys[K_DOWN] and pink_y < SCREEN_HEIGHT - 105:
-        pink_y += speed
+        pink_y += SPEED
 
-    display.update()
-    clock.tick(60)
+    display.update() # refrescando la pantalla
+    clock.tick(60) # estableciendo la tasa de refresco
 
 quit()
