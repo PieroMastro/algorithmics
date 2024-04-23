a25e823a2577ad14ba875a23b8b612160514e2b5
PieroMastro <pier.andres@gmail.com>
2024-04-23 15:55:16 +0000

    MapManager class refactored

--- Python Pro II/M5L2 Loading a Map/mapmanager.py
+++ Python Pro II/M5L2 Loading a Map/mapmanager.py
@@ -1,9 +1,9 @@
 class MapManager():
-    """Gestión del mapa"""
+    # Gestión del mapa
     def __init__(self):
-        self.model = 'block' # el modelo del cubo está en el archivo block.egg
-        # # se utilizan las siguientes texturas:
-        self.texture = 'block.png'         
+        self.model = 'block.egg'
+        # se utilizan las siguientes texturas:
+        self.texture = 'block.png'
         self.colors = [
             (0.2, 0.2, 0.35, 1),
             (0.2, 0.5, 0.2, 1),
@@ -11,16 +11,19 @@ class MapManager():
             (0.5, 0.3, 0.0, 1)
         ] #rgba
         # crea el nodo principal del mapa:
-        self.startNew()
+        self.land = render.attachNewNode("Land")
+
+        # self.startNew()
         # self.addBlock((0,10, 0))
 
 
-    def startNew(self):
-        """crea la base para un nuevo mapa"""
-        self .land = render.attachNewNode ( "Land" ) # el nodo al cual todos los bloques de mapa están vinculados
+    # def startNew(self):
+    #     # Crea la base para un nuevo mapa
+    #     self.land = render.attachNewNode ("Land") # el nodo al cual todos los bloques de mapa están vinculados
 
 
     def getColor(self, z):
+        #  Este método devuelve un color basado en la altura z del bloque
         if z < len(self.colors):
             return self.colors[z]
         else:
@@ -28,7 +31,7 @@ class MapManager():
 
 
     def addBlock(self, position):
-        # crear bloques de construcción
+        # Crea bloques de construcción y agrega al mapa en la posición especificada
         self.block = loader.loadModel(self.model)
         self.block.setTexture(loader.loadTexture(self.texture))
         self.block.setPos(position)
@@ -38,21 +41,36 @@ class MapManager():
 
 
     def clear(self):
-        """restablece el mapa"""
+        # Restablece el mapa, eliminando el nodo land y llamando a startNew() para crear un nuevo nodo
         self.land.removeNode()
-        self.startNew()
+        self.land = render.attachNewNode("Land")
+        # self.startNew()
 
 
     def loadLand(self, filename):
-        """crea un mapa de tierra desde un archivo de texto, devuelve sus dimensiones"""
+        # Limpia el mapa antes de cargar uno nuevo
         self.clear()
+
+        # Abre el archivo de texto que contiene el mapa
         with open(filename) as file:
-            y = 0
-            for line in file:
-                x = 0
-                line = line.split(' ')
-                for z in line:
-                    for z0 in range(int(z)+1):
-                        block = self.addBlock((x, y, z0))
-                    x += 1
-                y += 1
+            # Itera sobre cada línea en el archivo
+            for y, line in enumerate(file):
+                # Divide la línea en una lista de alturas de bloques
+                for x, height in enumerate(line.split()):
+                    # Agrega bloques en la posición (x, y, z) según la altura especificada en la línea
+                    for z in range(int(height) + 1):
+                        self.addBlock((x, y, z))
+
+    # def loadLand(self, filename):
+    #     # Crea un mapa de tierra desde un archivo de texto, devuelve sus dimensiones
+    #     self.clear()
+    #     with open(filename) as file:
+    #         y = 0
+    #         for line in file:
+    #             x = 0
+    #             line = line.split(' ')
+    #             for z in line:
+    #                 for z0 in range(int(z) + 1):
+    #                     block = self.addBlock((x, y, z0))
+    #                 x += 1
+    #             y += 1
