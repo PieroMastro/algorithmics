2842cf710d9cfa549bbe4f63b39dcbd32b5bc24f
PieroMastro <pier.andres@gmail.com>
2024-06-03 21:48:06 +0000

    Added main.py to "Projecto PyQt5"

--- Python Pro/Proyecto PyQt5/main.py
+++ Python Pro/Proyecto PyQt5/main.py
@@ -1,36 +1,36 @@
-#importando módulos y widgets
+# Importando módulos y widgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QPushButton, QLabel, QLineEdit
- 
-#declarando constantes
-win_width, win_height = 800, 300
-win_x, win_y = 200, 200
-txt_title = "Enviando texto"
-txt_send = "Enviar"
-txt_line = "Campo de entrada"
+
+# Declarando constantes
+WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
+POS_X, POS_Y = 200, 200
+TEXT_TITLE = "Enviando texto"
+TXT_SEND = "Enviar"
+TXT_LINE = "Campo de entrada"
 
 
 class MainWindow(QWidget):
     def __init__(self, parent=None, flags=Qt.WindowFlags()):
-        #llamando a un constructor de la clase padre
+        # Llamando a un constructor de la clase padre
         super().__init__(parent=parent, flags=flags)
 
-        #creando y personalizando los elementos gráficos
+    # Creando y personalizando los elementos gráficos
         self.initUI()
 
-        #conecta los elementos
+        # Conecta los elementos
         self.connects()
 
-        #determina cómo se verá la ventana (texto, tamaño, ubicación)
+        # Determina cómo se verá la ventana (texto, tamaño, ubicación)
         self.set_appear()
 
-        #inicio:
+        # Inicio:
         self.show()
 
     def initUI(self):
-        '''  crea elementos gráficos  '''
-        self.btn_send = QPushButton(txt_send, self)
-        self.line = QLineEdit(txt_line)
+        # Creando elementos graficos
+        self.btn_send = QPushButton(TXT_SEND, self)
+        self.line = QLineEdit(TXT_LINE)
         self.lable_finish = QLabel()
 
         self.layout_line = QHBoxLayout()
@@ -39,23 +39,21 @@ class MainWindow(QWidget):
         self.layout_line.addWidget(self.lable_finish, alignment = Qt.AlignCenter)          
         self.setLayout(self.layout_line)
 
-    
     def next_click(self):
         self.lable_finish.setText(self.line.text())
 
     def connects(self):
         self.btn_send.clicked.connect(self.next_click)
 
-    # determina cómo se verá la ventana (texto, tamaño, ubicación)
+    # Determina posicion/tamaño y caracteristicas de la vebtana
     def set_appear(self):
-        self.setWindowTitle(txt_title)
-        self.resize(win_width, win_height)
-        self.move(win_x, win_y)
-
+        self.setWindowTitle(TEXT_TITLE)
+        self.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.move(POS_X, POS_Y)
 
 def main():
     app = QApplication([])
-    mw = MainWindow()
+    main_window = MainWindow()
     app.exec_()
 
 main()
