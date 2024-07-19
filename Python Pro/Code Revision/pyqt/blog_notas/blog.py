86b73fc123c2deffdcc8065725c45cadc65ecfe4
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2024-07-19 15:50:13 -0400

    Update blog.py, added main interface

--- Python Pro/Code Revision/pyqt/blog_notas/blog.py
+++ Python Pro/Code Revision/pyqt/blog_notas/blog.py
@@ -1,45 +1,59 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QLineEdit
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton,
+                            QHBoxLayout, QVBoxLayout, QLineEdit, QTextEdit,
+                            QMessageBox)
 from PyQt5.QtCore import Qt
 
 
+with open('notes.txt','w') as file:
+    file.write('PRUEBA')
+    file.close()
+
+# FUNCIONALIDAD
+# Funcion para cargar una nota:
+def open_note():
+    pass
+
+# Funcion para salvar una nota:
+def save_note():
+    pass
+
+# CONSTANTES
+WINDOW_WIDTH, WINDOW_HEIGHT = 500, 600
+
+
+# MAIN WINDOW
 app = QApplication([])
 main_window = QWidget()
-main_window.resize(800, 600)
-
-main_window.setWindowTitle("app")
+main_window.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
+main_window.setWindowTitle("Notes App")
 
 main_window.show()
-#main_window.hide()
-
-file = open('notes.txt', 'w')
-file.write('Información')
-file.close()
-texto = QLabel("Hello World!", alignment = Qt.AlignCenter)
 
 
-boton = QPushButton("presionar")
-
-texto.hide()
-v_line.addWidget(texto)
-v_line.addWidget(boton)
-
-file.hide()
-v_line.addWidget(file)
+# WIDGETS
+nota_text_edit = QTextEdit()
+crear_nota_button = QPushButton("Crear nota")
+abrir_nota_button = QPushButton("Abrir nota")
+guardar_nota_button = QPushButton("Guardar nota")
 
 
+# LAYOUT
+layout_botones = QHBoxLayout()
+layout_botones.addWidget(crear_nota_button)
+layout_botones.addWidget(abrir_nota_button)
+layout_botones.addWidget(guardar_nota_button)
 
-def show_fun_title():
-    texto.show()
+layout_principal = QVBoxLayout()
+layout_principal.addLayout(layout_botones)
+layout_principal.addWidget(nota_text_edit)
 
-def mostrar_escrito():
-    file.show()
+main_window.setLayout(layout_principal)
 
 
 
 
 
 
-main_window.setLayout(v_line)
-boton.clicked.connect(show_fun_title)
+
 
 app.exec_()
