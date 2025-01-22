17b17f5aa2cee51bcf875dd43ba4d487b5c61283
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2025-01-22 13:48:47 -0400

    Update notes_v2.py

--- Python Start II/Smart Notes App/notes_v2.py
+++ Python Start II/Smart Notes App/notes_v2.py
@@ -1,47 +1,44 @@
+
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (
-    QApplication, QWidget, QHBoxLayout, QVBoxLayout,
-    QGroupBox, QButtonGroup, QRadioButton, QPushButton,
-    QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog, QFormLayout)
-    # importar nuevos metodos: QListWidget, QLineEdit, QTextEdit, QInputDialog, QFormLayout
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, 
+                            QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog)
+                            # importar nuevos metodos: QListWidget, QLineEdit, QTextEdit, QInputDialog
 import json
 
 app = QApplication([])
 
-#NOTAS EN JSON
-# No es necesario escribir el json file al inicio, lo manejamos a travez de funcionex vinculadas a los botones ahora
-
+# NOTAS EN JSON
+# Ya no hay necesidad de declarar nuestro archivo json, las funciones que creamos se encargan de 
 # notes = {
-#     'Bienvenido!' : {
-#         'texto' : 'Esta es la mejor app para tomar notas!',
-#         'etiquetas' : ['Accion 1', 'Accion 2', 'Accion 3']
+#     "¡Bienvenido!" : {
+#         "texto" : "¡Esta es la mejor app para tomar notas del mundo!",
+#         "etiquetas" : ["Bien", "Instrucciones"]
 #     }
 # }
-
-# with open('notes_data.json', 'w') as file:
+# with open("notes_data.json", "w") as file:
 #     json.dump(notes, file, ensure_ascii=False)
 
 #INTERFAZ DE LA APP
 # 1. Parametros de la app:
 main_window = QWidget()
-main_window.setWindowTitle('Notas inteligentes')
+main_window.setWindowTitle('App de Notas Inteligentes')
 main_window.resize(900, 600)
 
 # 2. Creando los widgets:
 list_notes = QListWidget()
-list_notes_label = QLabel('Lista de notas')
-create_note_btn = QPushButton('Crear nota')
-delete_note_btn = QPushButton('Borrar nota')
-save_note_btn = QPushButton('Guardar nota')
+list_notes_label = QLabel('Lista de Notas')
+create_note_btn = QPushButton('Crear Nota')  # al presionarlo debe aparecer una ventana emergente con el campo "Ingresar nombre de Nota"
+delete_note_btn = QPushButton('Borrar Nota')
+save_note_btn = QPushButton('Guardar Nota')
 
 field_tag = QLineEdit('')
-field_tag.setPlaceholderText('Ingresar etiqueta...')
+field_tag.setPlaceholderText('Ingresar Etiqueta...')
 field_text = QTextEdit()
-button_add = QPushButton('Añadir nota')
-button_del = QPushButton('Remover etiqueta de nota')
-button_search = QPushButton('Buscar notas por etiqueta')
+button_add = QPushButton('Añadir Nota')
+button_del = QPushButton('Remover Etiqueta de Nota')
+button_search = QPushButton('Buscar Notas por Etiqueta')
 list_tags = QListWidget()
-list_tags_label = QLabel('Lista de etiquetas')
+list_tags_label = QLabel('Lista de Etiquetas')
 
 # 3. Organizando el diseño de los widgets:
 notes_layout = QHBoxLayout()
@@ -75,35 +72,32 @@ notes_layout.addLayout(col_1, stretch = 2)
 notes_layout.addLayout(col_2, stretch = 1)
 main_window.setLayout(notes_layout)
 
-#FUNCIONALIDAD DE LA APP
-# trabajando con el texto de la nota
+
+# FUNCIONALIDAD DE LA APP
+
+# Obtener el texto de la nota con el título resaltado y mostrarlo en el campo de editar
+def show_note():
+    key = list_notes.selectedItems()[0].text()
+    print(key)
+    field_text.setText(notes[key]["texto"])
+    list_tags.clear()
+    list_tags.addItems(notes[key]["etiquetas"])
+    
+# Agregar una nueva nota
 def add_note():
-    # uutilizamos la ventana QInputdialog y creamos un elemento de notas, guardando en list_notes
-    note_name, ok = QInputDialog.getText(
-        main_window, "Añadir nota", "Nombre de nota: "
-    )
-    if ok and note_name != "":
+    note_name, ok = QInputDialog.getText(main_window, 'Agregar Nota', 'Nombre de la Nota')
+    if ok and note_name != '':
         notes[note_name] = {
-            'texto' : '',
-            'etiquetas' : []
+            'texto': '',
+            'etiquetas': []
         }
+
         list_notes.addItem(note_name)
-        list_tags.addItems(notes[note_name]['etiquetas'])
+        #list_tags.addItems(notes[note_name]['etiquetas'])
         print(notes)
 
-def save_note():
-    if list_notes.selectedItems():
-        key = list_notes.selectedItems()[0].text()
-        notes[key]['texto'] = field_text.toPlainText()
-        
-        with open('notes_data.json', 'w') as file:
-            json.dump(notes, file, ensure_ascii=False)
-            print(notes)
-    else:
-        print('¡Ninguna nota seleccionada!')
-
-def del_note():
-    # eliminamos la nota del diccionario y limpia los datos de la nota en los widgets:
+# Eliminar una nota
+def delete_note():
     if list_notes.selectedItems():
         key = list_notes.selectedItems()[0].text()
         del notes[key]
@@ -113,31 +107,34 @@ def del_note():
         list_notes.addItems(notes)
 
         with open('notes_data.json', 'w') as file:
-            json.dump(notes, file, ensure_ascii=False)
-            print(notes)
+            json.dump(notes, file, sort_keys=True, ensure_ascii=False)
+        print(notes)
+
     else:
-        print('¡Ninguna nota seleccionada!')
+        print('Por favor seleccione una nota para eliminar!')
+
+# Guardar una nota
+def save_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        notes[key]['texto'] = field_text.toPlainText()
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes, file, sort_keys=True, ensure_ascii=False)
+        print(notes)
+    else:
+        print('Por favor seleccione una nota para guardar!')
 
 
-def show_note():
-# obtener el texto de la nota con el título resaltado y mostrarlo en el campo de editar:
-    key = list_notes.selectedItems()[0].text()
-    print(key)
-    field_text.setText(notes[key]["texto"])
-    list_tags.clear()
-    list_tags.addItems(notes[key]["etiquetas"])
-
-# manejo de eventos:
+# 4. Ejecutando la app:
+# Procesamiento de evento de eventos
 create_note_btn.clicked.connect(add_note)
 list_notes.itemClicked.connect(show_note)
 save_note_btn.clicked.connect(save_note)
-delete_note_btn.clicked.connect(del_note)
+delete_note_btn.clicked.connect(delete_note)
 
-# 4. Ejecutando la app:
 main_window.show()
 
-# mostrar el archivo json:
-with open('notes_data.json', 'r') as file:
+with open("notes_data.json", "r") as file:
     notes = json.load(file)
 list_notes.addItems(notes)
 
