d6d8565868a4a22bfd454ed660832d582c55d76b
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2025-03-12 10:04:55 -0400

    Update mapmanager.py

--- Python Pro II/M5L3 Player Movement/mapmanager.py
+++ Python Pro II/M5L3 Player Movement/mapmanager.py
@@ -1,54 +1,54 @@
-class Mapmanager():
-    """Gestión del mapa"""
+from random import random
+
+# generate_colors = lambda: [
+#     (round(random(), 3), round(random(), 3), round(random(), 3), 1)
+#     for _ in range(4)
+#     ]
+
+def generate_colors():
+    colors = []
+    for i in range(4):
+        random_color = (round(random(), 3), round(random(), 3), round(random(), 3), 1)
+        colors.append(random_color)
+    return colors
+
+class MapManager():
     def __init__(self):
-        self.model = 'block' # el modelo del cubo está en el archivo block.egg
-        # se utilizan las siguientes texturas:
-        self.texture = 'block.png'         
-        self.colors = [
-            (0.2, 0.2, 0.35, 1),
-            (0.2, 0.5, 0.2, 1),
-            (0.7, 0.2, 0.2, 1),
-            (0.5, 0.3, 0.0, 1)
-        ] #rgba
-            # crea el nodo principal del mapa:
-        self.startNew()
-        # self.addBlock((0,10, 0))
-    
-    def startNew(self):
-        """crea la base para un nuevo mapa"""
-        self.land = render.attachNewNode("Land") # el nodo al cual todos los bloques de mapa están vinculados
-    
-    def getColor(self, z):
-        if z < len(self.colors):
-            return self.colors[z]
-        else:
-            return self.colors[len(self.colors) - 1]
-    
-    def addBlock(self, position):
-        # crear bloques de construcción
+        self.model = 'block.egg'
+        self.texture = 'block.png'
+        # self.color = random_color
+        self.colors = generate_colors()
+
+        self.start_new()
+        # self.add_block((0, 10, 0))
+
+    def start_new(self):
+        self.land = render.attachNewNode('Land')
+
+
+    def add_block(self, position):
         self.block = loader.loadModel(self.model)
         self.block.setTexture(loader.loadTexture(self.texture))
-        self.block.setPos(position)
-        self.color = self.getColor(int(position[2]))
+        self.color = self.get_color(position[2])
         self.block.setColor(self.color)
+        self.block.setPos(position)
         self.block.reparentTo(self.land)
-    
-    def clear(self):
-        """restablece el mapa"""
+
+    def load_land(self, filename):
+        self.clear_node()
+        with open(filename, 'r') as file:
+            for y, row in enumerate(file):
+                row = map(int, row.split(' '))
+                for x, col in enumerate(row):
+                    height = col + 1
+                    for z in range(height):
+                        self.add_block((x, y, z))
+
+    def clear_node(self):
         self.land.removeNode()
-        self.startNew()
-    
-    def loadLand(self, filename):
-        """crea un mapa de tierra desde un archivo de texto, devuelve sus dimensiones"""
-        self.clear()
-        with open(filename) as file:
-            y = 0
-            for line in file:
-                x = 0
-                line = line.split(' ')
-                for z in line:
-                    for z0 in range(int(z)+1):
-                        block = self.addBlock((x, y, z0))
-                    x += 1
-                y += 1
-        return x,y
+        self.start_new()
+
+    def get_color(self, z):
+        if z < len(self.colors):
+            return self.colors[z]
+        return self.colors[len(self.colors) - 1]
