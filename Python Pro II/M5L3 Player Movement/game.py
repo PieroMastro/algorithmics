a3e585e4e306b2bcc3290a6a18281ec8d767c2e2
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2025-03-12 10:05:51 -0400

    Update game.py

--- Python Pro II/M5L3 Player Movement/game.py
+++ Python Pro II/M5L3 Player Movement/game.py
@@ -2,16 +2,16 @@ from direct.showbase.ShowBase import ShowBase
 from mapmanager import MapManager
 from hero import Hero
 
-
 class Game(ShowBase):
     def __init__(self):
-        ShowBase.__init__(self)
+        super().__init__(self)
         self.land = MapManager()
-        self.land.loadLand("land.txt")
+        self.land.load_land('land.txt')
+        self.hero = Hero((1, 5, 1 ), self.land)
+        # x, y = self.land.load_land("land.txt")
+        # self.hero = Hero((x//2, y//2, 2), self.land)
 
-        self.hero = Hero((1, 25, 2), self.land)
         base.camLens.setFov(90)
-
 
 game = Game()
 game.run()
