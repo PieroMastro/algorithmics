a9aacb9393d410c5c3fbc58d8121d7c4f2d9e5f9
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2025-03-19 12:32:53 -0400

    Update mapmanager.py

--- Python Pro II/M5L4 Game Modes/mapmanager.py
+++ Python Pro II/M5L4 Game Modes/mapmanager.py
@@ -1,72 +1,72 @@
+
+"""GESTION DEL MAPA"""
+
 class Mapmanager():
-    """Gestión del mapa"""
     def __init__(self):
-        self.model = 'block' # el modelo del cubo está en el archivo block.egg
-        # # se utilizan las siguientes texturas:
-        self.texture = 'block.png'         
+        self.model = 'block' # asignar la geometria a nuestro modelo   
+        self.texture = 'block.png'  # asignar textura:     
         self.colors = [
             (0.2, 0.2, 0.35, 1),
             (0.2, 0.5, 0.2, 1),
             (0.7, 0.2, 0.2, 1),
             (0.5, 0.3, 0.0, 1)
         ] #rgba
+        
         # crea el nodo principal del mapa:
-        self.startNew()
-        # self.addBlock((0,10, 0))
+        self.start_new()
+        # self.add_block((0,10, 0)) reemplazada por un metodo para renderizar el mapa dinamicamente
 
 
-    def startNew(self):
-        """crea la base para un nuevo mapa"""
+    def start_new(self):
+        # crea la base para un nuevo mapa
         self .land = render.attachNewNode ( "Land" ) # el nodo al cual todos los bloques de mapa están vinculados
 
 
-    def getColor(self, z):
+    def get_color(self, z):
+        # selecciona el color basado en la altura del bloque
         if z < len(self.colors):
             return self.colors[z]
         else:
             return self.colors[len(self.colors) - 1]
 
 
-    def addBlock(self, position):
+    def add_block(self, position):
         # crear bloques de construcción
         self.block = loader.loadModel(self.model)
         self.block.setTexture(loader.loadTexture(self.texture))
         self.block.setPos(position)
-        self.color = self.getColor(int(position[2]))
+        self.color = self.get_color(int(position[2]))
         self.block.setColor(self.color)
-
         self.block.setTag("at", str(position))
-
+        
         self.block.reparentTo(self.land)
 
 
-    def clear(self):
-        """restablece el mapa"""
+    def clear_node(self):
+        # reestablece el mapa
         self.land.removeNode()
-        self.startNew()
+        self.start_new()
 
 
-    def loadLand(self, filename):
-        """crea un mapa de tierra desde un archivo de texto, devuelve sus dimensiones"""
-        self.clear()
+    def load_land(self, filename):
+        # crea un mapa de tierra desde un archivo de texto, devuelve sus dimensiones
+        self.clear_node()
         with open(filename) as file:
-            y = 0
-            for line in file:
-                x = 0
-                line = line.split(' ')
-                for z in line:
-                    for z0 in range(int(z)+1):
-                        block = self.addBlock((x, y, z0))
-                    x += 1
-                y += 1
-        return x,y
+            for y, row in enumerate(file):
+                row = map(int, row.split(' '))
+                for x, col in enumerate(row):
+                    height = col + 1
+                    for z in range(height):
+                        self.add_block((x, y, z))
+        return x, y
     
-    def findBlocks(self, pos):
+    def find_blocks(self, pos):
+        # encuentra todos los bloques en el nodo
         return self.land.findAllMatches("=at=" + str(pos))
 
 
-    def isEmpty(self, pos):
-        blocks = self.findBlocks(pos)
+    def is_empty(self, pos):
+        blocks = self.find_blocks(pos)
         if blocks:
             return False
         else:
@@ -76,28 +76,28 @@ class Mapmanager():
     def findHighestEmpty(self, pos):
         x, y, z = pos
         z = 1
-        while not self.isEmpty((x, y, z)):
+        while not self.is_empty((x, y, z)):
             z += 1
         return (x, y, z)
 
 
-    def buildBlock(self, pos):
-        """Colocamos el bloque, considerando la gravedad:"""
+    def build_block(self, pos):
+        # olocamos el bloque, considerando la gravedad
         x, y, z = pos
         new = self.findHighestEmpty(pos)
         if new[2] <= z + 1:
-            self.addBlock(new)
+            self.add_block(new)
 
 
-    def delBlock(self, position):
-        """elimina bloques en la posición especificada""" 
-        blocks = self.findBlocks(position)
+    def delete_block(self, position):
+        # elimina bloques en la posición especificada
+        blocks = self.find_blocks(position)
         for block in blocks:
             block.removeNode()
 
 
-    def delBlockFrom(self, position):
+    def delete_block_from(self, position):
         x, y, z = self.findHighestEmpty(position)
         pos = x, y, z - 1
-        for block in self.findBlocks(pos):
+        for block in self.find_blocks(pos):
                 block.removeNode()
