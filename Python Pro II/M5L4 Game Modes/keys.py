47600ee085a1b2f92328ac52ebda41ceab4983ee
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2025-03-19 12:25:32 -0400

    Create keys.py

---
+++ Python Pro II/M5L4 Game Modes/keys.py
@@ -0,0 +1,25 @@
+
+'''Teclas asignadas a funcionalidad'''
+
+key_switch_camera = 'c' # la cámara está ligada al héroe o no
+key_switch_mode = 'z' # puede atravesar obstáculos o no
+
+
+key_forward = 'w' # avanzar (la dirección a la que apunta la cámara)
+key_back = 's' # retroceder
+key_left = 'a' # caminar a la izquierda (de lado a la cámara)
+key_right = 'd' # caminar a la derecha
+key_up = 'e' # subir
+key_down = 'q' # bajar
+
+
+key_turn_left = 'n' # girar la cámara a la derecha (y el mundo a la izquierda)
+key_turn_right = 'm' # girar la cámara a la izquierda (y el mundo a la derecha)
+
+
+key_build = 'b' # construir un bloque frente a ti
+key_destroy = 'v' # destruir el bloque frente a ti
+
+
+key_load = 'l' # cargar un mapa
+key_save = 'k' # guardar un mapa
