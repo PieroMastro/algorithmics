37b09037f503e8be8b9e35c0d35be8b27083b182
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2025-03-22 07:15:43 -0400

    Update mapmanager.py

--- Python Pro II/M5L3 Player Movement/mapmanager.py
+++ Python Pro II/M5L3 Player Movement/mapmanager.py
@@ -16,39 +16,35 @@ class MapManager():
     def __init__(self):
         self.model = 'block.egg'
         self.texture = 'block.png'
-        # self.color = random_color
         self.colors = generate_colors()
-
         self.start_new()
         # self.add_block((0, 10, 0))
-
+    
     def start_new(self):
         self.land = render.attachNewNode('Land')
-
-
+    
     def add_block(self, position):
         self.block = loader.loadModel(self.model)
-        self.block.setTexture(loader.loadTexture(self.texture))
         self.color = self.get_color(position[2])
-        self.block.setColor(self.color)
+        self.block.setTexture(loader.loadTexture(self.texture), 1)
         self.block.setPos(position)
+        self.block.setColor(self.color)
         self.block.reparentTo(self.land)
 
     def load_land(self, filename):
         self.clear_node()
         with open(filename, 'r') as file:
-            for y, row in enumerate(file):
-                row = map(int, row.split(' '))
-                for x, col in enumerate(row):
-                    height = col + 1
-                    for z in range(height):
+            for y, line in enumerate(file):
+                columns = map(int, line.split())
+                for x, col in enumerate(columns):
+                    for z in range(col + 1):
                         self.add_block((x, y, z))
-
+                        
     def clear_node(self):
         self.land.removeNode()
         self.start_new()
-
-    def get_color(self, z):
-        if z < len(self.colors):
-            return self.colors[z]
-        return self.colors[len(self.colors) - 1]
+        
+    def get_color(self, pos_z):
+        if pos_z < len(self.colors):
+            return self.colors[pos_z]
+        return self.colors[len(colors) - 1]
