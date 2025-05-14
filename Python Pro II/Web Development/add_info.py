19217fa6d75b4cbfae3c0c8ccf85ab2e902e2cec
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2025-05-14 10:11:56 -0400

    Update add_info.py

--- Python Pro II/Web Development/add_info.py
+++ Python Pro II/Web Development/add_info.py
@@ -1,13 +1,14 @@
 import sqlite3
 
-db_name = 'questions.db'
+db_name = 'quizzes.db'
 conn = None
 cursor = None
 
-def open(): 
+def start(): 
     global conn, cursor
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
+    cursor.execute('PRAGMA foreign_keys = ON')
 
 def close():
     cursor.close()
@@ -17,25 +18,31 @@ def do(query):
     conn.execute(query)
     conn.commit()
 
+def clear_db():
+    start()
+    cursor.execute('PRAGMA foreign_keys = OFF')
+    do('DROP TABLE IF EXISTS quiz')
+    do('DROP TABLE IF EXISTS question')
+    do('DROP TABLE IF EXISTS quiz_content')
+    close()
+
 def create_tables():
-    open()
+    start()
 
     do('''CREATE TABLE IF NOT EXISTS quiz (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL
-        )'''
-    )
+        )''')
 
     do(
         '''CREATE TABLE IF NOT EXISTS question (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            question TEXT NOT NULL,
+            name TEXT NOT NULL,
             answer TEXT NOT NULL,
             incorrect_option_1 TEXT NOT NULL,
             incorrect_option_2 TEXT NOT NULL,
             incorrect_option_3 TEXT NOT NULL
-        )'''
-    )
+        )''')
 
     do('''CREATE TABLE IF NOT EXISTS quiz_content (
             id INTEGER PRIMARY KEY,
@@ -43,8 +50,7 @@ def create_tables():
             question_id INTEGER,
             FOREIGN KEY (quiz_id) REFERENCES quiz (id),
             FOREIGN KEY (question_id) REFERENCES question (id)
-        )'''
-    )
+        )''')
 
     close()
 
@@ -58,30 +64,76 @@ def add_question():
         ('¿Qué es más grande que un elefante y no pesa nada?', 'La sombra de un elefante','Un globo','Un paracaídas', 'Una nube')
     ]
 
-    open()
-    cursor.executemany('''INSERT INTO question (question, answer, incorrect_option_1, incorrect_option_2, incorrect_option_3)
+    start()
+    cursor.executemany('''INSERT INTO question (name, answer, incorrect_option_1, incorrect_option_2, incorrect_option_3)
             VALUES (?,?,?,?,?)''', questions)
     conn.commit()
     close()
 
-create_tables()
-add_question()
-
-open() # Abrir la conexión antes del SELECT
-cursor.execute('SELECT * FROM question')
-data = cursor.fetchall()
-print(data)
-
 def add_quiz():
     quizzes = [
-        ('Own game'),
-        ('¿Quién quiere ser millonario?'),
-        ('El más inteligente')
+        ('Own game',),
+        ('¿Quién quiere ser millonario?',),
+        ('El más inteligente',)
     ]
-    open()
+    start()
     cursor.executemany('''INSERT INTO quiz (name) VALUES (?)''', quizzes)
     conn.commit()
     close()
 
 def add_links():
-    pass
+    start()
+    query = '''
+        INSERT INTO quiz_content (quiz_id, question_id)
+        VALUES (?, ?)
+    '''
+    link = input('Desea agregar una pregunta a un cuestionario? (y/n)')
+    while link.lower() == 'y':
+        try:
+            quiz_id = int(input('Ingrese el id del cuestionario: '))
+            question_id = int(input('Ingrese el id de la pregunta: '))
+            cursor.execute(query, [quiz_id, question_id])
+            conn.commit()
+        except ValueError:
+            print('Ingrese un número válido')
+        finally:
+            link = input('Desea agregar una pregunta a un cuestionario? (y/n)')
+    close()
+
+def get_question_after(question_id=0, quiz_id=1):
+    start()
+    query = '''
+        SELECT quiz_content.id, question.name, question.answer, question.incorrect_option_1, question.incorrect_option_2, question.incorrect_option_3
+        FROM quiz_content, question
+        WHERE quiz_content.question_id = question.id
+        AND quiz_content.id > ? AND quiz_content.quiz_id = ?
+        ORDER BY quiz_content.id
+        LIMIT 1'''
+
+    cursor.execute(query, [question_id, quiz_id])
+    result = cursor.fetchone()
+    close()
+    return result
+
+def show(table):
+    start()
+    query = f'SELECT * FROM {table}'
+    cursor.execute(query)
+    print(cursor.fetchall())
+    close()
+
+def show_tables():
+    show('quiz')
+    show('question')
+    show('quiz_content')
+
+def main():
+    clear_db()
+    create_tables()
+    add_question()
+    add_quiz()
+    add_links()
+    show_tables()
+
+if __name__ == '__main__':
+    main()
