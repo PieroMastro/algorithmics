8fa6112b4b1d941747a75a8a7a8799917c6ec490
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2025-05-21 15:30:17 -0400

    Update quiz_session.py

--- Python Pro II/Web Development/quiz_session.py
+++ Python Pro II/Web Development/quiz_session.py
@@ -1,25 +1,24 @@
 from flask import Flask, session, redirect, url_for
-from queries_db import get_next_question
-from random import randint
+from db_scripts import get_next_question
 
+app = Flask(__name__)
 
 def index():
-    max_quizzes = 3
-    session['quiz'] = 1 #randint(1, max_quizzes) change to randint later
-    session['prev_question'] = 0
+    session['quiz'] = 1
+    session['current_question'] = 0
 
-    return "<h1>Hola Flask</h1><a href=/test>Click para iniciar!</a>"
+    return "<h1>Bienvenido a la app de cuestionarios!</h1><a href=/test>Click para iniciar!</a>"
 
 
 def test():
     quiz = session['quiz']
-    prev_question = session['prev_question']
-    result = get_next_question(prev_question, quiz)
+    current_question = session['current_question']
+    result = get_next_question(current_question, quiz)
 
     if result is None or len(result) == 0:
         return redirect(url_for('result'))
     else:
-        session['prev_question'] = result[0]
+        session['current_question'] = result[0]
         return f"<h2>Quiz: {session['quiz']}</h2><h3>Pregunta: {result}</h3>"
 
 
@@ -27,7 +26,6 @@ def result():
     return f"<h2>Resultado del Quiz {session['quiz']}:</h2>"
 
 
-app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ThisIsSecret'
 app.add_url_rule('/', 'index', index)
 app.add_url_rule('/test', 'test', test)
