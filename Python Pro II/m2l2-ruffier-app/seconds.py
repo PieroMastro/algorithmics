6dfe21eb98cea8f18ea3196ccead0f158daffe7a
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2025-11-14 12:20:11 -0500

    Refactor Seconds class and update timer text

--- Python Pro II/m2l2-ruffier-app/seconds.py
+++ Python Pro II/m2l2-ruffier-app/seconds.py
@@ -1,7 +1,6 @@
-from kivy.uix.label import Label
 from kivy.clock import Clock
+from kivy.uix.label import Label
 from kivy.properties import BooleanProperty
-
 
 class Seconds(Label):
     done = BooleanProperty(False)
@@ -9,18 +8,17 @@ class Seconds(Label):
     def __init__(self, total, **kwargs):
         self.total = total
         self.current = 0
-        self.done = False
-        sec_text = f'Segundos transcurridos: {self.current}'
-        super().__init__(text=sec_text, **kwargs)
+        timer_text = f'Segundos transcurridos: {self.current}'
+        super().__init__(text=timer_text, **kwargs)
 
     def start(self):
         Clock.schedule_interval(self.counter, 1)
 
-    def restart(self, total):
+    def restart(self, total, *args):
         self.total = total
         self.current = 0
         self.done = False
-        sec_text = f'Segundos transcurridos: {self.current}'
+        timer_text = f'Segundos transcurridos: {self.current}'
         self.start()
 
     def counter(self, dt):
