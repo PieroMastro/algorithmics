66b0ba5c32a0bce9c0c2d04b1796663905ccc05b
Piero | Mastrogiacomo <110270525+PieroMastro@users.noreply.github.com>
2026-02-05 14:41:42 -0400

    Refactor data cleaning and model training process

--- Python Pro II/M4L1 Machine Learning/digital_edu_clean.py
+++ Python Pro II/M4L1 Machine Learning/digital_edu_clean.py
@@ -6,46 +6,54 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 
 
 df = pd.read_csv('train.csv')
+columns_to_drop = [
+    'id', 'bdate', 'graduation', 'education_status',
+    'last_seen', 'city', 'occupation_name', 'career_start',
+    'career_end', 'langs', 'people_main'
+]
 
-df.drop(['id','bdate','graduation', 'education_status',
-        'last_seen', 'city','occupation_name', 'career_start',
-        'career_end','langs', 'people_main'], axis=1, inplace= True)
+df.drop(columns=colums_to_drop, inplace=True)
 
-df['education_form'].fillna('Full-time', inplace= True)
-df['occupation_type'].fillna('university', inplace = True)
+df['education_form'].fillna('Full-time', inplace=True)
+df['occupation_type'].fillna('university', inplace=True)
+
 
 def replace_false(data):
-    if data=='False':
+    if data == 'False':
         return int(9)
     else:
         return int(data)
+
+
 df['life_main'] = df['life_main'].apply(replace_false)
+
 
 def change_occupation(data):
     if data == 'university':
         return 1
     return 0
+
+
 df['occupation_type'] = df['occupation_type'].apply(change_occupation)
 
-df[list(pd.get_dummies(df['education_form']).columns)] = pd.get_dummies(df['education_form'])
-df.drop('education_form', axis=1, inplace=True)
+df = pd.get_dummies(df, columns=['education_form'])
 
-print(df.info())
+df.info()
 
 # MODEL
-x = df.drop('result', axis=1)
+x = df.drop(columns='result')
 y = df['result']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-sc = StandardScaler()
-x_train = sc.fit_transform(x_train)
-x_test = sc.transform(x_test)
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
 
 classifier = KNeighborsClassifier(n_neighbors=5)
 classifier.fit(x_train, y_train)
 
-y_predict = classifier.predict(x_test)
+prediction = classifier.predict(x_test)
 
-accuracy = accuracy_score(y_test, y_predict) * 100
+accuracy = accuracy_score(y_test, prediction) * 100
 print(f'Porcentaje de acierto: {round(accuracy, 2)}%')
