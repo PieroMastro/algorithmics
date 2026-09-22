8ecf4f6b8f71b2e5fb865c2bbd9cb3ef0a3d25be
Piero | Mastrogiacomo <pier.andres@gmail.com>
2026-09-22 13:56:34 -0400

    Create instructions module for Ruffier Test
    Added constants and instructions for the Ruffier Test application.

---
+++ Python Pro/app-escritorio/instructions.py
@@ -0,0 +1,15 @@
+# Módulo de constanstes y data
+WIN_WIDTH, WIN_HEIGHT = 1000, 600
+WIN_X, WIN_Y = 200, 100
+
+TXT_TITLE = 'Ruffier Test'
+TXT_HELLO = '¡Bienvenido al Programa de detección de estado de salud!'
+TXT_NEXT = 'Iniciar'
+TXT_INSTRUCTION = ('Esta aplicación le permite usar la prueba de Ruffier para realizar un diagnóstico inicial de su salud.\n'
+                   'La prueba de Ruffier es un conjunto de ejercicios físicos diseñado para evaluar su rendimiento cardíaco durante el esfuerzo físico.\n'
+                   'El sujeto se tumba en posición supina durante 5 minutos y se toma la frecuencia del pulso durante 15 segundos; \n'
+                   'Luego, dentro de 45 segundos, el sujeto realiza 30 sentadillas.\n'
+                   'Cuando el ejercicio termina, el sujeto se tumba y se toman sus pulsaciones de nuevo durante 15 segundos\n'
+                   'y luego durante los últimos 15 segundos del primer minuto del periodo de recuperación.\n'
+                   '¡Importante! Si no se siente bien durante la prueba (mareos,\n'
+                   'tinnitus, falta de respiración, etc.), detenga la prueba y consulte con un médico.')
