16b1fd9cb1bcc82fdcf90e2b908af88ce554ee3a
Piero | Mastrogiacomo <pier.andres@gmail.com>
2026-09-23 00:00:08 -0400

    Refactor instructional texts in instructions.py
    Updated instructional texts for the Ruffier Test application, improving clarity and formatting.

--- Python Pro/app-escritorio/instructions.py
+++ Python Pro/app-escritorio/instructions.py
@@ -1,15 +1,33 @@
-# Módulo de constanstes y data
+# Dimensiones y posición de las ventanas
 WIN_WIDTH, WIN_HEIGHT = 1000, 600
 WIN_X, WIN_Y = 200, 100
 
+# Textos de MainWindow
 TXT_TITLE = 'Ruffier Test'
 TXT_HELLO = '¡Bienvenido al Programa de detección de estado de salud!'
 TXT_NEXT = 'Iniciar'
-TXT_INSTRUCTION = ('Esta aplicación le permite usar la prueba de Ruffier para realizar un diagnóstico inicial de su salud.\n'
-                   'La prueba de Ruffier es un conjunto de ejercicios físicos diseñado para evaluar su rendimiento cardíaco durante el esfuerzo físico.\n'
-                   'El sujeto se tumba en posición supina durante 5 minutos y se toma la frecuencia del pulso durante 15 segundos; \n'
-                   'Luego, dentro de 45 segundos, el sujeto realiza 30 sentadillas.\n'
-                   'Cuando el ejercicio termina, el sujeto se tumba y se toman sus pulsaciones de nuevo durante 15 segundos\n'
-                   'y luego durante los últimos 15 segundos del primer minuto del periodo de recuperación.\n'
-                   '¡Importante! Si no se siente bien durante la prueba (mareos,\n'
-                   'tinnitus, falta de respiración, etc.), detenga la prueba y consulte con un médico.')
+TXT_INSTRUCTION = (
+    'Esta aplicación le permite usar la prueba de Ruffier para realizar un diagnóstico inicial de su salud.\n'
+    'La prueba de Ruffier es un conjunto de ejercicios físicos diseñado para evaluar su rendimiento cardíaco.\n'
+    'El sujeto se tumba en posición supina durante 5 minutos y se toma la frecuencia del pulso durante 15 segundos;\n'
+    'Luego, dentro de 45 segundos, el sujeto realiza 30 sentadillas.\n'
+    'Cuando el ejercicio termina, se toman sus pulsaciones de nuevo durante 15 segundos\n'
+    'y luego durante los últimos 15 segundos del primer minuto de recuperación.\n'
+    '¡Importante! Si no se siente bien durante la prueba, deténgala y consulte con un médico.'
+)
+
+# Textos de TestWindow
+TXT_TEST_TITLE = 'Realizando la prueba de Ruffier'
+TXT_AGE = 'Ingrese su edad:'
+TXT_TEST1 = 'Mida su pulso durante 15 segundos (P1):'
+TXT_TEST2 = 'Realice 30 sentadillas en 45 segundos y mida su pulso (P2):'
+TXT_TEST3 = 'Descanse 1 minuto y mida su pulso en los últimos 15 segundos (P3):'
+TXT_START_TEST1 = 'Iniciar primer test'
+TXT_START_TEST2 = 'Iniciar sentadillas'
+TXT_START_TEST3 = 'Iniciar test final'
+TXT_SEND_RESULTS = 'Enviar resultados'
+
+# Textos de ResultWindow
+TXT_RESULT_TITLE = 'Resultado del Diagnóstico'
+TXT_INDEX = 'Su índice de Ruffier es: '
+TXT_WORKOUT = 'Rendimiento cardíaco: '
